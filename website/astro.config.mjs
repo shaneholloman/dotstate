b90e53b8862976add75d7794453afdec4173c97a
Serkan Yersen <serkanyersen@gmail.com>
2026-01-22 19:19:04 -0800

    Upgrade the website to be more managable

---
+++ website/astro.config.mjs
@@ -0,0 +1,10 @@
+import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel';
+
+export default defineConfig({
+  output: 'static',
+  adapter: vercel(),
+  build: {
+    assets: 'assets'
+  }
+});
